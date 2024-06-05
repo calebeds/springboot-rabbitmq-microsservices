3d0d1002c4287dcc1a5c913a0686de638fafbb07
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-06-05 20:54:13 -0300

    first commit

---
+++ email-service/src/test/java/me/calebe_oliveira/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.calebe_oliveira.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
