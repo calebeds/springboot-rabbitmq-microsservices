5b50af26fe961d1c650f14aeb0e0932d091196b1
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-06-06 21:23:55 -0300

    implement sending event to email queue

--- order-service/src/main/java/me/calebe_oliveira/order_service/publisher/OrderProducer.java
+++ order-service/src/main/java/me/calebe_oliveira/order_service/publisher/OrderProducer.java
@@ -11,17 +11,26 @@ import org.springframework.stereotype.Service;
 public class OrderProducer {
     private final String exchangeName;
     private final String orderRoutingKey;
+    private final String emailRoutingKey;
     private final RabbitTemplate rabbitTemplate;
 
     public OrderProducer(@Value("${rabbitmq.exchange.name}") String exchangeName,
-                         @Value("${rabbitmq.binding.routing.key}") String orderRoutingKey, RabbitTemplate rabbitTemplate) {
+                         @Value("${rabbitmq.binding.routing.key}") String orderRoutingKey,
+                         @Value("${rabbitmq.binding.email.routing.key}") String emailRoutingKey,
+                         RabbitTemplate rabbitTemplate) {
         this.exchangeName = exchangeName;
         this.orderRoutingKey = orderRoutingKey;
+        this.emailRoutingKey = emailRoutingKey;
         this.rabbitTemplate = rabbitTemplate;
     }
 
     public void sendMessage(OrderEvent orderEvent) {
         log.info(String.format("Order event sent to RabbitMQ -> %s", orderEvent.toString()));
+
+        // send an order event to order queue
         rabbitTemplate.convertAndSend(exchangeName, orderRoutingKey, orderEvent);
+
+        // send an order event to email queue
+        rabbitTemplate.convertAndSend(exchangeName, emailRoutingKey, orderEvent);
     }
 }
