1590de28e69c615686ad5ebed73dd8b29e2a3d00
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-06-06 21:30:26 -0300

    configure rabbitmq email service and add consumer

---
+++ email-service/src/main/java/me/calebe_oliveira/email_service/consumer/OrderConsumer.java
@@ -0,0 +1,18 @@
+package me.calebe_oliveira.email_service.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import me.calebe_oliveira.email_service.dto.OrderEvent;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class OrderConsumer {
+
+    @RabbitListener(queues = "${rabbitmq.queue.email.name=}")
+    public void consume(OrderEvent orderEvent) {
+        log.info(String.format("Order event received in email service -> %s", orderEvent.toString()));
+
+        // email service needs to send an email to customer
+    }
+}
---
+++ email-service/src/main/java/me/calebe_oliveira/email_service/dto/Order.java
@@ -0,0 +1,27 @@
+package me.calebe_oliveira.email_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Order {
+    private String orderId;
+    private String name;
+    private Integer qty;
+    private Double price;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", name='" + name + '\'' +
+                ", qty=" + qty +
+                ", price=" + price +
+                '}';
+    }
+}
